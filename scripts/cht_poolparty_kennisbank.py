58b71121dc12578604a7d35b0dfbaeef6aa10021
RubenSchalk <rubenschalk1@gmail.com>
2024-06-27 12:52:34 +0200

    update kennisbank query with topconcepts

--- scripts/cht_poolparty_kennisbank.py
+++ scripts/cht_poolparty_kennisbank.py
@@ -4,6 +4,7 @@ import requests
 query = """
 PREFIX skos: <http://www.w3.org/2004/02/skos/core#>
 PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+
 CONSTRUCT {
   ?subject rdf:type ?type .
   ?subject skos:prefLabel ?prefLabel .
@@ -12,32 +13,44 @@ CONSTRUCT {
   ?subject skos:hiddenLabel ?hiddenLabel .
   ?subject skos:broader ?broader .
   ?subject skos:narrower ?narrower .
-
   ?subject skos:inScheme ?scheme .
-  ?scheme skos:hasTopConcept ?subject .
-  ?subject skos:topConceptOf ?scheme .
-  ?scheme rdf:type skos:ConceptScheme .
+  ?scheme skos:hasTopConcept ?topConcept .
+  ?topConcept skos:topConceptOf ?scheme .
 }
-
 WHERE {
-  GRAPH <https://data.cultureelerfgoed.nl/term/id/cht/thesaurus> {
-   
-    ?subject rdf:type ?type ;
-             skos:prefLabel ?prefLabel ;
-             skos:scopeNote ?scopeNote .
-    FILTER (?type = skos:Concept && LANG(?prefLabel) = 'nl' && LANG(?scopeNote) = 'nl')
-    OPTIONAL { ?subject skos:altLabel ?altLabel . FILTER(LANG(?altLabel) = 'nl') }
-    OPTIONAL { ?subject skos:hiddenLabel ?hiddenLabel . FILTER(LANG(?hiddenLabel) = 'nl') }
-    OPTIONAL { ?subject skos:broader ?broader }
-    OPTIONAL { ?subject skos:narrower ?narrower }
- 
- ?subject skos:inScheme ?scheme .
- ?scheme a skos:ConceptScheme .
-optional{ ?scheme skos:hasTopConcept ?subject } .
-optional{ ?subject skos:topConceptOf ?scheme .}
-     
+  {
+    SELECT DISTINCT ?subject ?type ?prefLabel ?scopeNote ?narrower ?broader ?altLabel ?hiddenLabel ?scheme
+    WHERE {
+      GRAPH <https://data.cultureelerfgoed.nl/term/id/cht/thesaurus> {
+        ?subject rdf:type ?type ;
+                 skos:prefLabel ?prefLabel ;
+                 skos:scopeNote ?scopeNote .
+        FILTER (?type = skos:Concept && LANG(?prefLabel) = 'nl' && LANG(?scopeNote) = 'nl')
+
+        OPTIONAL { ?subject skos:altLabel ?altLabel . FILTER(LANG(?altLabel) = 'nl') }
+        OPTIONAL { ?subject skos:hiddenLabel ?hiddenLabel . FILTER(LANG(?hiddenLabel) = 'nl') }
+        OPTIONAL { ?subject skos:broader ?broader }
+        OPTIONAL { ?subject skos:narrower ?narrower }
+
+        ?subject skos:inScheme ?scheme .
+        ?scheme a skos:ConceptScheme .
+        FILTER(?scheme = <https://data.cultureelerfgoed.nl/term/id/cht/b532325c-dc08-49db-b4f1-15e53b037ec3>)
+      }
+    }
   }
-} 
+  UNION
+  {
+    SELECT DISTINCT ?scheme ?topConcept
+    WHERE {
+      GRAPH <https://data.cultureelerfgoed.nl/term/id/cht/thesaurus> {
+        ?scheme a skos:ConceptScheme .
+        ?scheme skos:hasTopConcept ?topConcept .
+        ?topConcept skos:topConceptOf ?scheme .
+        FILTER(?scheme = <https://data.cultureelerfgoed.nl/term/id/cht/b532325c-dc08-49db-b4f1-15e53b037ec3>)
+      }
+    }
+  }
+}
 """
 
 # Endpoint URL
